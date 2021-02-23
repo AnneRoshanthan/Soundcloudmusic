daa2ad9d889dd1e695ac3fe7cf5ef3b790c04e6a
AnneRoshanthan <anneroshanthan@gmail.com>
2021-02-24 00:00:04 +0530

    Music Player

--- main.js
+++ main.js
@@ -5,19 +5,74 @@ SC.initialize({
   
   $(document).ready(function() {
 
-    SC.stream('/tracks/967114759',function(sound){
-        $('#start').click(function(e) {
+    SC.stream('/tracks/752544118',function(sound){
+        $('#start1').click(function(e) {
             e.preventDefault();
             sound.start();
           });
 
-          $('#stop').click(function(e) {
+          $('#stop1').click(function(e) {
             e.preventDefault();
             sound.stop();
           });
- 
-    });
+        });
+        SC.stream('/tracks/371818226',function(sound){
+          $('#start2').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
 
+          $('#stop2').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          }); 
+        });
+
+          SC.stream('/tracks/784800565',function(sound){
+          $('#start3').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+        });
+
+          SC.stream('/tracks/489403410',function(sound){
+          $('#start4').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop4').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+        });
+          SC.stream('/tracks/217615268',function(sound){
+          $('#start5').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop5').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+        });
+
+          SC.stream('/tracks/871718233',function(sound){
+          $('#start6').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop6').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+
+});
     }); 
-
-    // <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/967114759&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/indianpop-00092901" title="Da Ua" target="_blank" style="color: #cccccc; text-decoration: none;">Da Ua</a> · <a href="https://soundcloud.com/indianpop-00092901/master-the-blaster-bjorn-surrao-official-mp3" title="Master the Blaster - Bjorn Surrao (Official Mp3)" target="_blank" style="color: #cccccc; text-decoration: none;">Master the Blaster - Bjorn Surrao (Official Mp3)</a></div>
